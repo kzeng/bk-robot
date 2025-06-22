d313353b5f023f917062a9e6f9d416b4d8f8671b
zengkai001 <zengkai001@qq.com>
2025-06-22 16:31:35 +0800

    Enable allow_unsafe_werkzeug option in socketio.run for development purposes

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ app = create_app()
 
 if __name__ == '__main__':
     logger.info("Starting bk-robot application...")
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
