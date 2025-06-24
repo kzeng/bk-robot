901b067a8132c7afd74fa834693946b5750e17e2
zengkai <zengkai001@qq.com>
2025-06-24 19:58:45 +0800

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-# Last config update: 2025-06-23 15:29:44
+# Last config update: 2025-06-24 19:07:56
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
